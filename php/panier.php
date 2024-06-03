0814953ac027415426ddab9a3c188e6457bec52d
Laury <renaulaury@gmail.com>
2024-06-03 22:11:36 +0200

    Rajout panier.html et mep header

--- php/panier.php
+++ php/panier.php
@@ -13,7 +13,11 @@
 
 <body>
     <header>
-        <nav id="navbar">      
+        <nav id="navbar">
+            <div id="blockLogo">
+                <img id="logo" src="../images/logo.png">
+            </div>
+
             <div class="burger_button">
                 <div class="line"></div>
                 <div class="line"></div>
@@ -21,35 +25,23 @@
             </div>
 
             <ul>
-                <li><a href="../index.html">Accueil</a></li>
-                <li><a href="./tickets.html">Tickets</a></li>                
-                <li><a href="./localisation.html">Nous trouver</a></li>
-                <li><a href="./events23.html">Evenements 2023</a></li>
-                <li><a href="./contact.html">Contact</a></li>
+                <li><a href="../index.html">Accueil</a></li>      
+                <li><a href="localisation.html">Nous trouver</a></li>                            
+                <li><a href="events23.html">Evenements 2023</a></li>                  
+                <li><a href="contact.html">Nous contacter</a></li> 
             </ul>
         </nav>
-
-        <div id="blockLogo">
-            <img id="logo" src="../images/logo.png">                
-        </div>
-        
-        <div id="blockPanier">
-            <a href="./panier.html">
-                <img id="panier" src="../images/panier.png">
-            </a>
-        </div>
     </header>
 
     <main>
         <section class="blockPrinc">
             <div class="bande">
             </div>
-            <h1>Récap</h1>
+            <h1>Tickets</h1>
         </section>      
         
         <section class="block">
-            <h2>Récap</h2>
-            
+            <h2>Panier</h2>
             
         </section>
     </main>
